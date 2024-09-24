27d633c495e8ddf637e37146ca01b8c64158a958
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-24 13:12:19 -0400

    update

--- src/DaysInMonth.java
+++ src/DaysInMonth.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class DaysInMonth {
+public class DaysInMonth {
+
+    public static void main(String[] args) {
+        System.out.println(getDaysInMonth(1, 2020));
+        System.out.println(getDaysInMonth(2, 2020));
+        System.out.println(getDaysInMonth(2, 2018));
+        System.out.println(getDaysInMonth(-1, 2020));
+        System.out.println(getDaysInMonth(2, -2020));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) {
+            return -1;
+        }
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+
+        switch (month) {
+            case 1: // January
+            case 3: // March
+            case 5: // May
+            case 7: // July
+            case 8: // August
+            case 10: // October
+            case 12: // December
+                return 31; // 31 days
+            case 4: // April
+            case 6: // June
+            case 9: // September
+            case 11: // November
+                return 30; // 30 days
+            case 2: // February
+                if (isLeapYear(year)) {
+                    return 29; // Leap year
+                }
+                return 28; // Non-leap year
+            default:
+                return -1; // Should not reach here
+        }
+    }
 }
